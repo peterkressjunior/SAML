1e6e5cffa731f9fb4a26aa746b785d12e32a6af9
peterkressjunior <peterkress@mail.de>
2017-04-23 21:51:48 +0200

    Update TrustedIssuer.cs

--- TrustedIssuer.cs
+++ TrustedIssuer.cs
@@ -1,10 +1,8 @@
-using System;
-
 namespace PeterKressJunior.Saml  
 {
     /// <summary>
     /// Stores name and SSL certificate thumbprint 
-    /// of a Trusted Issuer for comming SAML Assertions.
+    /// of a Trusted Issuer for comming SAML2 Assertions.
     /// </summary>
     internal class TrustedIssuer
     {
